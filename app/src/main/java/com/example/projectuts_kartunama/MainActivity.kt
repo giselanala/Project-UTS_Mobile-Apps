2bdfd71026ef62abd6c3ae2e43da3bc4153ebe92
giselanala <giselakuliah@gmail.com>
2025-10-10 14:05:09 +0700

    Ubah logo android

--- app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
+++ app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,12 +45,15 @@ fun KartuNamaLayoutKosong() {
         verticalArrangement = Arrangement.Center
     ) {
         Image(
-            painter = painterResource(id = R.drawable.android), // nama file di drawable, tanpa ekstensi .png
+            painter = painterResource(id = R.drawable.androidd),
             contentDescription = "Logo Android",
             modifier = Modifier.size(120.dp)
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
     }
-}
+    }
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun KartuNamaLayoutKosongPreview() {
