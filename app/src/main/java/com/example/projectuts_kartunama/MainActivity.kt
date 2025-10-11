34daf3edee991614aa057e495afcc7e0312a54b7
devigiovanii <giovaniiidevi@gmail.com>
2025-10-11 20:08:30 +0700

    Buat nama pada kartu nama

--- app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
+++ app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,6 +43,7 @@ fun KartuNamaLayoutKosong() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        // Logo Android
         Image(
             painter = painterResource(id = R.drawable.androidd),
             contentDescription = "Logo Android",
@@ -51,8 +51,17 @@ fun KartuNamaLayoutKosong() {
         )
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        // Nama
+        Text(
+            text = "Gisela Febriana",
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
     }
-    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -61,4 +70,3 @@ fun KartuNamaLayoutKosongPreview() {
         KartuNamaLayoutKosong()
     }
 }
-
