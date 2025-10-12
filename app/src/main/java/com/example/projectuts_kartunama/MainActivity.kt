ac733d238b7b3f5b2ff1ea7d2771c99851a1d867
giselanala <giselakuliah@gmail.com>
2025-10-12 23:17:15 +0700

    ubah ikon + font,bentuk logo

--- app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
+++ app/src/main/java/com/example/projectuts_kartunama/MainActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
 import com.example.projectuts_kartunama.ui.theme.ProjectUts_KartuNamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +44,7 @@ fun KartuNamaLayoutKosong() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
+            .padding(horizontal = 48.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -48,7 +52,10 @@ fun KartuNamaLayoutKosong() {
         Image(
             painter = painterResource(id = R.drawable.androidd),
             contentDescription = "Logo Android",
-            modifier = Modifier.size(120.dp)
+            modifier = Modifier
+                .size(180.dp)
+                .padding(bottom = 5.dp)
+                .clip(RoundedCornerShape(30.dp))
         )
 
         Spacer(modifier = Modifier.height(24.dp))
@@ -56,6 +63,7 @@ fun KartuNamaLayoutKosong() {
         // Nama
         Text(
             text = "Gisela Febriana",
+            fontFamily = FontFamily.Serif,
             style = MaterialTheme.typography.headlineMedium,
             color = Color.Black
         )
@@ -67,6 +75,7 @@ fun KartuNamaLayoutKosong() {
             style = MaterialTheme.typography.headlineMedium,
             color = Color.Green,
             fontSize = 17.sp,
+            fontFamily = FontFamily.Serif,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
 
@@ -77,19 +86,22 @@ fun KartuNamaLayoutKosong() {
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 32.dp, top = 4.dp, end = 24.dp)
+                .padding(start = 32.dp, top = 4.dp, end = 24.dp),
+
         ) {
             Image(
-                painter = painterResource(id = R.drawable.android_logo),
-                contentDescription = "android_logo",
+                painter = painterResource(id = R.drawable.telepon),
+                contentDescription = "telepon",
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(45.dp)
                     .padding(end = 12.dp)
             )
             Text(
                 text = "+62 821 6298 3537",
                 fontSize = 15.sp,
-                color = Color.Black
+                color = Color.Black,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.fillMaxWidth(),
             )
         }
 
@@ -103,16 +115,18 @@ fun KartuNamaLayoutKosong() {
                 .padding(start = 32.dp, top = 4.dp, end = 24.dp)
         ) {
             Image(
-                painter = painterResource(id = R.drawable.android_berbagi),
-                contentDescription = "android_berbagi",
+                painter = painterResource(id = R.drawable.berbagi),
+                contentDescription = "berbagi",
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(45.dp)
                     .padding(end = 12.dp)
             )
             Text(
                 text = "@giselafebriana",
                 fontSize = 15.sp,
+                fontFamily = FontFamily.Serif,
                 color = Color.Black
+
             )
         }
 
@@ -129,13 +143,15 @@ fun KartuNamaLayoutKosong() {
                 painter = painterResource(id = R.drawable.android_email),
                 contentDescription = "android_email",
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(45.dp)
                     .padding(end = 12.dp)
             )
             Text(
                 text = "gisela.febriana@gmail.com",
                 fontSize = 15.sp,
+                fontFamily = FontFamily.Serif,
                 color = Color.Black
+
             )
         }
     }
